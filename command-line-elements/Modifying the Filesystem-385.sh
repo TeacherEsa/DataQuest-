d17ba423631ccd84c2c4823cea23b39e1e836040
TeacherEsa <50472196+TeacherEsa@users.noreply.github.com>
2020-05-27 22:15:22 +0800

    Add files via upload

---
+++ command-line-elements/Modifying the Filesystem-385.sh
@@ -0,0 +1,23 @@
+## 1. Creating Directories ##
+
+/home/dq$ mkdir dir2 dir3 dir5
+
+## 2. Deleting Directories ##
+
+/home/dq$ rmdir dir2 dir3 dir5
+
+## 4. Copying Files ##
+
+/home/dq$ cp augustus violet veruca tv brats
+
+## 6. Copying Directories ##
+
+/home/dq$ cp -R prize_winners/ brats/
+
+## 7. Deleting Directories and Files ##
+
+/home/dq$ cd
+
+## 8. Moving and Renaming Directories and Files ##
+
+/home/dq$ mv /home/dq/brats/prize_winners/.mike /home/dq/brats/prize_winners/Mik
---
+++ command-line-elements/Users and Permissions-387.sh
@@ -0,0 +1,35 @@
+## 1. Users ##
+
+/home/dq$ id
+
+## 2. Groups ##
+
+/home/dq$ groups
+
+## 3. Revisiting ls ##
+
+/$ cd root
+
+## 4. Permissions ##
+
+/$ answer y y n y n
+
+## 5. Changing Permissions: Symbolic Notation ##
+
+/home/dq$ chmod a=rwx,o-x d*
+
+## 6. The File Status Command ##
+
+/home/dq$ stat /home/dq/Trash
+
+## 7. Changing Permissions: Octal Notation ##
+
+/home/dq$ answer 602 171 ---r--r-x
+
+## 8. The Superuser ##
+
+/home/dq$ cal
+
+## 9. Changing Ownership ##
+
+/home/dq$ history
